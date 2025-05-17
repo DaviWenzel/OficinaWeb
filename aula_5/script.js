84cd6b0fe7e18926c0fb652db6a80a0d9990217d
Davi Wenzel <daviwenzel25@gmail.com>
2025-05-16 22:49:21 -0300

    Trabalho final

--- aula_5/script.js
+++ aula_5/script.js
@@ -1,238 +1,110 @@
 function imprime(objeto) {
-    var mensagem = `
-  <br>
-  {
-    <br>
-    "<span class='campo'>id</span>": ${objeto.id},
-    <br>
-    "<span class='campo'>userId</span>": ${objeto.userId},
-    <br>
-    "<span class='campo'>title</span>": ${objeto.title},
-    <br>
-    "<span class='campo'>body</span>": ${objeto.body}
-    <br>
+  if (Array.isArray(objeto)) {
+    return objeto
+      .map(
+        (obj) => `
+      <br>
+      {
+        <br>
+        "<span class='campo'>id</span>": ${obj.id},
+        <br>
+        "<span class='campo'>userId</span>": ${obj.userId},
+        <br>
+        "<span class='campo'>title</span>": ${obj.title},
+        <br>
+        "<span class='campo'>body</span>": ${obj.body}
+        <br>
+      }
+      <br>`
+      )
+      .join("");
+  } else {
+    return `
+      <br>
+      {
+        <br>
+        "<span class='campo'>id</span>": ${objeto.id},
+        <br>
+        "<span class='campo'>userId</span>": ${objeto.userId},
+        <br>
+        "<span class='campo'>title</span>": ${objeto.title},
+        <br>
+        "<span class='campo'>body</span>": ${objeto.body}
+        <br>
+      }
+      <br>`;
   }
-  <br>`;
-
-    return mensagem;
-  }
+}
 
 async function getPostagem(id) {
-    await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        respjson.innerHTML = imprime(json);
-      });
-  }
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const json = await response.json();
+  console.log(json);
+  respjson.innerHTML = imprime(json);
+}
 
-  function imprimeMensagem(objeto) {
-    var mensagem;
+async function getPostagens() {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const json = await response.json();
+  console.log(json);
+  respjson.innerHTML = imprime(json);
+}
 
-    objeto.forEach(function (obj) {
-      mensagem += `
-    <br>
-    {
-      <br>
-      "<span class='campo'>id</span>": ${obj.id},
-      <br>
-      "<span class='campo'>userId</span>": ${obj.userId},
-      <br>
-      "<span class='campo'>title</span>": ${obj.title},
-      <br>
-      "<span class='campo'>body</span>": ${obj.body}
-      <br>
+async function postPostagens() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
     },
-    <br>`;
-    });
+    body: JSON.stringify({
+      title: "Nova Postagem",
+      body: "Essa é a minha mais nova postagem! :D",
+      userId: 1,
+    }),
+  });
+  const json = await response.json();
+  console.log(json);
+  respjson.innerHTML = imprime(json);
+}
 
-    return mensagem;
-  }
-
-  async function getPostagensSemID() {
-    await fetch(`https://jsonplaceholder.typicode.com/posts`)
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        respjson.innerHTML = imprime(json);
-      });
-  }
-
-
-  function imprime(objeto) {
-    var mensagem = `
-  <br>
-  {
-    <br>
-    "<span class='campo'>id</span>": ${objeto.id},
-    <br>
-    "<span class='campo'>userId</span>": ${objeto.userId},
-    <br>
-    "<span class='campo'>title</span>": ${objeto.title},
-    <br>
-    "<span class='campo'>body</span>": ${objeto.body}
-    <br>
-  }
-  <br>`;
-
-    return mensagem;
-  }
-
-  async function postPostagens() {
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-      body: JSON.stringify({
-        title: "Nova Postagem",
-        body: "Essa é a minha mais nova postagem! :D",
-        userId: 1,
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        respjson.innerHTML = imprime(json);
-      });
-  }
-
-  
-  function imprime(objeto) {
-    var mensagem = `
-  <br>
-  {
-    <br>
-    "<span class='campo'>id</span>": ${objeto.id},
-    <br>
-    "<span class='campo'>userId</span>": ${objeto.userId},
-    <br>
-    "<span class='campo'>title</span>": ${objeto.title},
-    <br>
-    "<span class='campo'>body</span>": ${objeto.body}
-    <br>
-  }
-  <br>`;
-
-    return mensagem;
-  }
-
-  async function atualizarPostagens() {
-    fetch("https://jsonplaceholder.typicode.com/posts/1", {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-      body: JSON.stringify({
-        id: 1,
-        title: "Minha Mais Nova Postagem",
-        body: "Essa é a atualização da minha mais nova postagem! :D",
-        userId: 1,
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        respjson.innerHTML = imprime(json);
-      });
-  }
-
-  
-  function imprime(objeto) {
-    var mensagem = `
-  <br>
-  {
-    <br>
-    "<span class='campo'>id</span>": ${objeto.id},
-    <br>
-    "<span class='campo'>userId</span>": ${objeto.userId},
-    <br>
-    "<span class='campo'>title</span>": ${objeto.title},
-    <br>
-    "<span class='campo'>body</span>": ${objeto.body}
-    <br>
-  }
-  <br>`;
-
-    return mensagem;
-  }
-
-  async function modificarPostagens(id) {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-      body: JSON.stringify({
-        title: "Essa é a modificação da minha mais nova postagem! :D",
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        respjson.innerHTML = imprime(json);
-      });
-  }
-
-  
-  function imprime(objeto) {
-    var mensagem = `
-  <br>
-  {
-    <br>
-    "<span class='campo'>id</span>": ${objeto.id},
-    <br>
-    "<span class='campo'>userId</span>": ${objeto.userId},
-    <br>
-    "<span class='campo'>title</span>": ${objeto.title},
-    <br>
-    "<span class='campo'>body</span>": ${objeto.body}
-    <br>
-  }
-  <br>`;
-
-    return mensagem;
-  }
-
-  async function removerPostagem(id) {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-      method: "DELETE",
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        respjson.innerHTML = imprime(json);
-      });
-  }
-
-  function imprime(objeto) {
-    var mensagem;
-
-    objeto.forEach(function (obj) {
-      mensagem += `
-    <br>
-    {
-      <br>
-      "<span class='campo'>id</span>": ${obj.id},
-      <br>
-      "<span class='campo'>userId</span>": ${obj.userId},
-      <br>
-      "<span class='campo'>title</span>": ${obj.title},
-      <br>
-      "<span class='campo'>body</span>": ${obj.body}
-      <br>
+async function atualizarPostagens() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
     },
-    <br>`;
-    });
+    body: JSON.stringify({
+      id: 1,
+      title: "Minha Mais Nova Postagem",
+      body: "Essa é a atualização da minha mais nova postagem! :D",
+      userId: 1,
+    }),
+  });
+  const json = await response.json();
+  console.log(json);
+  respjson.innerHTML = imprime(json);
+}
 
-    return mensagem;
-  }
+async function modificarPostagens(id) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      title: "Essa é a modificação da minha mais nova postagem! :D",
+    }),
+  });
+  const json = await response.json();
+  console.log(json);
+  respjson.innerHTML = imprime(json);
+}
 
-  async function filtrarPostagens(id) {
-    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        respjson.innerHTML = imprime(json);
-      });
-  }
+async function removerPostagem(id) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    method: "DELETE",
+  });
+  const json = await response.json();
+  console.log(json);
+  respjson.innerHTML = imprime(json);
+}
